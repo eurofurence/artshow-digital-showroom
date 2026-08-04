58fca6f58688c7b4a2af8217d31200731289d422
Maraxi <Maraxi@users.noreply.github.com>
2026-08-04 17:24:01 +0200

    server serve media

--- internal/server/server.go
+++ internal/server/server.go
@@ -30,5 +30,8 @@ func (s *Server) Start() error {
 	fs := http.FileServer(http.Dir("./web/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	fs = http.FileServer(http.Dir(s.cfg.Folder))
+	mux.Handle("/media/", http.StripPrefix("/media/", fs))
+
 	return http.ListenAndServe(s.addr, mux)
 }
