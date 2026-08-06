53254d451ba4c3ce68fae398082ee929fd2be5af
Maraxi <Maraxi@users.noreply.github.com>
2026-08-06 23:13:04 +0200

    support darwin in nix flake

--- dev/flake.nix
+++ dev/flake.nix
@@ -4,35 +4,42 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs @ {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          go
-          air  # hot reloading go server
-          golangci-lint  # fmt and lint go code
-          ffmpeg  # generate preview videos
-          imagemagick  # generate images
-        ];
+  outputs = inputs @ {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            air # hot reloading go server
+            golangci-lint # fmt and lint go code
+            ffmpeg # generate preview videos
+            imagemagick # generate images
+            mpv # playback
+          ];
 
-        shellHook = let
-          red_text = ''\e[0;31m'';
-          green_text = ''\e[0;32m'';
-          reset_formatting = ''\e[0m'';
-        in ''
-          echo -e "\
-          ${red_text}go version: ${green_text}$(go version)
-          ${red_text}air version: ${green_text}$(air -v)
-          ${red_text}golangci-lint version: ${green_text}$(golangci-lint version)
-          ${red_text}ffmpeg version: ${green_text}$(ffmpeg -version)
-          ${red_text}magick version: ${green_text}$(magick -version)
-          ${reset_formatting}"'';
-      };
-    };
-  };
+          shellHook = let
+            red_text = ''\e[0;31m'';
+            green_text = ''\e[0;32m'';
+            reset_formatting = ''\e[0m'';
+          in ''
+            echo -e "\
+            ${red_text}go version: ${green_text}$(go version)
+            ${red_text}air version: ${green_text}$(air -v)
+            ${red_text}golangci-lint version: ${green_text}$(golangci-lint version)
+            ${red_text}ffmpeg version: ${green_text}$(ffmpeg -version)
+            ${red_text}magick version: ${green_text}$(magick -version)
+            ${red_text}mpv version: ${green_text}$(magick --version)
+            ${reset_formatting}"'';
+        };
+      }
+    );
 }
