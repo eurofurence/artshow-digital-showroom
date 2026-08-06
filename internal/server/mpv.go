af18623154175ab9e28c3fab3fa70a74be06f442
Maraxi <Maraxi@users.noreply.github.com>
2026-08-06 17:42:40 +0200

    fix fmt

--- internal/server/mpv.go
+++ internal/server/mpv.go
@@ -1,15 +1,11 @@
 package server
 
-func (s *Server) sendCommandToMpv(commands ...[]any) error {
-	cmd := map[string]any{"command": commands}
-	return s.mpvEncoder.Encode(cmd)
-}
 func (s *Server) sendCommandsToMpv(cmds ...[]any) error {
-    for _, cmd := range cmds {
+	for _, cmd := range cmds {
 		command := map[string]any{"command": cmd}
 		if err := s.mpvEncoder.Encode(command); err != nil {
-            return err
-        }
-    }
-    return nil
+			return err
+		}
+	}
+	return nil
 }
