fab00cb322c1101c795f2d40f426328a3dd91593
Maraxi <Maraxi@users.noreply.github.com>
2026-08-08 01:58:00 +0200

    remove old statushandler

--- internal/server/server.go
+++ internal/server/server.go
@@ -88,31 +88,3 @@ func (s *Server) Close() error {
 
 	return nil
 }
-
-func (s *Server) StatusHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
-		return
-	}
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-r.Context().Done():
-			log.Println("client disconnected")
-			return
-
-		case t := <-ticker.C:
-			// log.Printf("data: %s\n\n", t.Format(time.TimeOnly))
-			fmt.Fprintf(w, "data: %s\n\n", t.Format(time.TimeOnly))
-			flusher.Flush()
-		}
-	}
-}
