480294b8cb6bd8a3649e1cd414121268bdcbede8
Maraxi <Maraxi@users.noreply.github.com>
2026-08-08 01:50:36 +0200

    add dialog handler

---
+++ internal/server/handler_dialog.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"log"
+	"net/http"
+)
+
+func (s *Server) DialogHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("DialogHandler got request for", r.Method, r.URL.Path)
+
+	id := r.PathValue("id")
+
+	video, ok := s.videos[id]
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+
+	if err := templates.ExecuteTemplate(w, "dialog", video); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
