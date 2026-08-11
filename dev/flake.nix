fcd2fab540bf34ff3269071fc15310b2d530f6a6
Maraxi <Maraxi@users.noreply.github.com>
2026-08-11 12:41:42 +0200

    nixfmt

--- dev/flake.nix
+++ dev/flake.nix
@@ -7,15 +7,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs @ {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in {
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             go
@@ -26,19 +29,21 @@
             mpv # playback
           ];
 
-          shellHook = let
-            red_text = ''\e[0;31m'';
-            green_text = ''\e[0;32m'';
-            reset_formatting = ''\e[0m'';
-          in ''
-            echo -e "\
-            ${red_text}go version: ${green_text}$(go version)
-            ${red_text}air version: ${green_text}$(air -v)
-            ${red_text}golangci-lint version: ${green_text}$(golangci-lint version)
-            ${red_text}ffmpeg version: ${green_text}$(ffmpeg -version)
-            ${red_text}magick version: ${green_text}$(magick -version)
-            ${red_text}mpv version: ${green_text}$(mpv --version)
-            ${reset_formatting}"'';
+          shellHook =
+            let
+              red_text = ''\e[0;31m'';
+              green_text = ''\e[0;32m'';
+              reset_formatting = ''\e[0m'';
+            in
+            ''
+              echo -e "\
+              ${red_text}go version: ${green_text}$(go version)
+              ${red_text}air version: ${green_text}$(air -v)
+              ${red_text}golangci-lint version: ${green_text}$(golangci-lint version)
+              ${red_text}ffmpeg version: ${green_text}$(ffmpeg -version)
+              ${red_text}magick version: ${green_text}$(magick -version)
+              ${red_text}mpv version: ${green_text}$(mpv --version)
+              ${reset_formatting}"'';
         };
       }
     );
