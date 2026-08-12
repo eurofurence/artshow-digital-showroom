224b14f2faf2ba41b78c0e39a6fc050bd5563263
Maraxi <Maraxi@users.noreply.github.com>
2026-08-12 14:27:28 +0200

    move fallback image

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -2,6 +2,6 @@ package constants
 
 const (
 	PlaybackTitleDuration = 10_000 // time in ms
-	FallbackImage         = "/static/fallback-lyca-shocked-bw.png"
+	FallbackImage         = "/static/images/fallback-lyca-shocked.png"
 	StandbyVideo          = "/static/videos/Art_Show_DVD.mp4"
 )
